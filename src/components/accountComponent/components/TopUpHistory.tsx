9e69ae987062e37420b38d761d7c82a0d004800d
egileekada <egileoniso.ekada@gmail.com>
2022-06-27 23:08:24 +0100

    update on tank

--- src/components/accountComponent/components/TopUpHistory.tsx
+++ src/components/accountComponent/components/TopUpHistory.tsx
@@ -79,16 +79,16 @@ export default function TopUpHistory(props: any) {
                         </Tr>
                     </Thead>
                     <Tbody >
-                        {[...data.data.stocks].reverse().filter((item: any)=> item.user !== null).slice(from-1, to).map((item:any , index: any)=> {
+                        {[...data.data.stocks].filter((item: any)=> item.product !== null && item.user !== null && item?.isAdding === true).reverse().slice(from-1, to).map((item:any , index: any)=> {
                             return(
                                 <Tr className=' cursor-pointer font-Inter-Regular text-sm ' key={index} paddingBottom='30px' >
-                                    <Td>{[...data.data.stocks].reverse().filter((item: any)=> item.user !== null).map((object: any) => object._id).indexOf(item._id)+1}</Td> 
+                                    <Td>{[...data.data.stocks].reverse().filter((item: any)=>  item.product !== null && item.user !== null && item?.isAdding === true).map((object: any) => object._id).indexOf(item._id)+1}</Td> 
                                     <Td>
                                         {item?.tank[0]?.tankId} 
                                     </Td> 
                                     <Td>
                                         {/* <div className='w-8' > */}
-                                            {item.product.productCode}
+                                            {item.product?.productCode}
                                         {/* </div> */}
                                     </Td> 
                                     <Td>
@@ -115,14 +115,14 @@ export default function TopUpHistory(props: any) {
 
             {!isLoading && (
                 <>    
-                    {limit < data.data.stocks.filter((item: any)=> item.user !== null).length && (
+                    {limit < data.data.stocks.filter((item: any)=>  item.product !== null && item.user !== null && item?.isAdding === true).length && (
 
                         <div className='flex items-center mt-6' >
                             <button onClick={()=> PrevPage()} style={{borderColor: '#C2C2C2'}} className='w-10 h-10 rounded-lg cursor-pointer flex justify-center items-center border' > 
                                 <IoIosArrowBack color='#878787' />
                             </button>
                                 <div style={{borderColor: '#C2C2C2'}} className='w-auto h-10 font-Graphik-Bold rounded-lg flex border mx-2'> 
-                                    {[...data.data.stocks].filter((item: any)=> item.user !== null).reverse().filter((item: any, index: any)=> index % limit === 0).map((item: any, index: any)=> {
+                                    {[...data.data.stocks].filter((item: any)=>  item.product !== null && item.user !== null && item?.isAdding === true).reverse().filter((item: any, index: any)=> index % limit === 0).map((item: any, index: any)=> {
                                         if(index <= 10){
                                             return( 
                                                 <div onClick={()=> OnTabPage(index+1)} style={tabIndex=== index+1 ? {backgroundColor: '#3E3F41'}:{color: '#202020'}} className='w-10 cursor-pointer h-10 rounded-lg flex text-white justify-center items-center' >
@@ -132,7 +132,7 @@ export default function TopUpHistory(props: any) {
                                         }  
                                     })} 
                                 </div>
-                            <button disabled={to >= data.data.stocks.filter((item: any)=> item.user !== null).length ? true: false} onClick={()=> NextPage()} style={{borderColor: '#C2C2C2'}} className='w-10 h-10 rounded-lg cursor-pointer flex justify-center items-center border' >
+                            <button disabled={to >= data.data.stocks.filter((item: any)=>  item.product !== null && item.user !== null && item?.isAdding === true).length ? true: false} onClick={()=> NextPage()} style={{borderColor: '#C2C2C2'}} className='w-10 h-10 rounded-lg cursor-pointer flex justify-center items-center border' >
                                 <IoIosArrowForward color='#878787' />
                             </button>
                         </div>
