ca46d8faec1994b1e2542acd93e4c20df8f524ac
Confidence Yobo <confiyobo@gmail.com>
2023-01-05 11:02:05 +0100

    Update DealNotification.tsx

--- src/components/DealComponent/component/DealNotification.tsx
+++ src/components/DealComponent/component/DealNotification.tsx
@@ -3,7 +3,7 @@ import { useQuery } from 'react-query'
 
 export default function DealNotification() {
     const { isLoading, data } = useQuery('AllDeals', () =>
-        fetch('http://faad-env.eba-kfucwakm.eu-central-1.elasticbeanstalk.com/api/v1/deals', {
+        fetch('https://obscure-oasis-95161.herokuapp.com/api/v1/deals', {
             method: 'GET', // or 'PUT'
             headers: {
                 'Content-Type': 'application/json', 
