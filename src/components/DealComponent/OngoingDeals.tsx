cd6c3d2d713e13947c5deac2183d1f94031f17d5
Confidence Yobo <confiyobo@gmail.com>
2023-01-05 08:14:35 +0100

    Update OngoingDeals.tsx

--- src/components/DealComponent/OngoingDeals.tsx
+++ src/components/DealComponent/OngoingDeals.tsx
@@ -8,7 +8,7 @@ export default function OngoingDeals(props: any) {
  
     const userContext: IUser = React.useContext(UserContext); 
     const { isLoading, data, refetch } = useQuery('AllDeals', () =>
-        fetch('https://faadoli.herokuapp.com/api/v1/deals', {
+        fetch('http://faad-env.eba-kfucwakm.eu-central-1.elasticbeanstalk.com/api/v1/deals', {
             method: 'GET', // or 'PUT'
             headers: {
                 'Content-Type': 'application/json', 
