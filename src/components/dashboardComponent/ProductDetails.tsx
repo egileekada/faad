1343feac5485d9f9b2f675dc972f17281e748b89
Confidence Yobo <confiyobo@gmail.com>
2023-01-05 09:01:45 +0100

    Update ProductDetails.tsx

--- src/components/dashboardComponent/ProductDetails.tsx
+++ src/components/dashboardComponent/ProductDetails.tsx
@@ -5,7 +5,7 @@ import PageLoader from '../PageLoader'
 export default function ProductDetails() { 
     
     const { isLoading, data } = useQuery('AllProductAndPricing', () =>
-        fetch('https://faadoli.herokuapp.com/api/v1/product', {
+        fetch('http://faad-env.eba-kfucwakm.eu-central-1.elasticbeanstalk.com/api/v1/product', {
             method: 'GET', // or 'PUT'
             headers: {
                 'Content-Type': 'application/json', 
