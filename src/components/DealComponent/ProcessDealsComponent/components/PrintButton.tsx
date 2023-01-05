dc6b1d7272b73df344b7847986322a5fc3573726
Confidence Yobo <confiyobo@gmail.com>
2023-01-05 08:21:42 +0100

    Update PrintButton.tsx

--- src/components/DealComponent/ProcessDealsComponent/components/PrintButton.tsx
+++ src/components/DealComponent/ProcessDealsComponent/components/PrintButton.tsx
@@ -17,7 +17,7 @@ const PrintButton = (props: any) => {
   const [truck, setTruckInfo] = React.useState('')
 
   if(props.agent){  
-    fetch(`https://faadoli.herokuapp.com/api/v1/auth/profile/all`, {
+    fetch(`http://faad-env.eba-kfucwakm.eu-central-1.elasticbeanstalk.com/api/v1/auth/profile/all`, {
         method: 'GET', // or 'PUT'
         headers: {
             'Content-Type': 'application/json',
@@ -46,7 +46,7 @@ const PrintButton = (props: any) => {
   }
   
   if(props.truck){  
-    fetch(`https://faadoli.herokuapp.com/api/v1/truck/${props.truck}`, {
+    fetch(`http://faad-env.eba-kfucwakm.eu-central-1.elasticbeanstalk.com/api/v1/truck/${props.truck}`, {
         method: 'GET', // or 'PUT'
         headers: {
             'Content-Type': 'application/json',
