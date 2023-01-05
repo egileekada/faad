a7a46abdad3e3eba520b43cedd1a603939f58d3d
Confidence Yobo <confiyobo@gmail.com>
2023-01-05 10:38:04 +0100

    Update EntryPermit.tsx

--- src/components/accountComponent/EntryPermit.tsx
+++ src/components/accountComponent/EntryPermit.tsx
@@ -61,7 +61,7 @@ export default function EntryPermit() {
 
 
     const { isLoading, data, refetch } = useQuery('Permit', () =>
-        fetch('http://faad-env.eba-kfucwakm.eu-central-1.elasticbeanstalk.com/api/v1/permit', {
+        fetch('https://obscure-oasis-95161.herokuapp.com/api/v1/permit', {
             method: 'GET', // or 'PUT'
             headers: {
                 'Content-Type': 'application/json', 
