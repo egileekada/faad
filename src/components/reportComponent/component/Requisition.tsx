4e140240a6ba80479dcf85b1fc6687239aea940f
Confidence Yobo <confiyobo@gmail.com>
2023-01-05 10:20:56 +0100

    Update Requisition.tsx

--- src/components/reportComponent/component/Requisition.tsx
+++ src/components/reportComponent/component/Requisition.tsx
@@ -4,7 +4,7 @@ import { useQuery } from 'react-query'
 export default function Requisition(props: any) { 
     
     const { isLoading, data } = useQuery('AllRequisition', () =>
-        fetch(`http://faad-env.eba-kfucwakm.eu-central-1.elasticbeanstalk.com/api/v1/requisition`, {
+        fetch(`https://obscure-oasis-95161.herokuapp.com/api/v1/requisition`, {
             method: 'GET', // or 'PUT'
             headers: {
                 'Content-Type': 'application/json', 
