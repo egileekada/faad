9cd9be8e08bbe9048152b3dff61000e307049a56
Confidence Yobo <confiyobo@gmail.com>
2023-01-05 10:22:01 +0100

    Update DealsDetail.tsx

--- src/components/dashboardComponent/DealsDetail.tsx
+++ src/components/dashboardComponent/DealsDetail.tsx
@@ -7,7 +7,7 @@ export default function DealsDetail() {
   
     const navigate = useNavigate()
     const { isLoading, data } = useQuery('AllDeals', () =>
-        fetch('http://faad-env.eba-kfucwakm.eu-central-1.elasticbeanstalk.com/api/v1/deals', {
+        fetch('https://obscure-oasis-95161.herokuapp.com/api/v1/deals', {
             method: 'GET', // or 'PUT'
             headers: {
                 'Content-Type': 'application/json', 
