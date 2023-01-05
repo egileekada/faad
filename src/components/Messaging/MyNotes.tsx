c12dfe4f0ca480cee049cc1cd647bb4e02fb4ac5
Confidence Yobo <confiyobo@gmail.com>
2023-01-05 08:26:08 +0100

    Update MyNotes.tsx

--- src/components/Messaging/MyNotes.tsx
+++ src/components/Messaging/MyNotes.tsx
@@ -34,7 +34,7 @@ export default function MyNotes() {
     });
 
     const { isLoading, data, refetch, } = useQuery('Note', () =>
-        fetch('https://faadoli.herokuapp.com/api/v1/auth/profile', {
+        fetch('http://faad-env.eba-kfucwakm.eu-central-1.elasticbeanstalk.com/api/v1/auth/profile', {
             method: 'GET', // or 'PUT'
             headers: {
                 'Content-Type': 'application/json', 
@@ -59,7 +59,7 @@ export default function MyNotes() {
           return;
         }
         else {
-            const request = await fetch(`https://faadoli.herokuapp.com/api/v1/note`, {
+            const request = await fetch(`http://faad-env.eba-kfucwakm.eu-central-1.elasticbeanstalk.com/api/v1/note`, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
