d51510edea7c3811b6b3e4a451695f6c9ef70160
Confidence Yobo <confiyobo@gmail.com>
2023-01-05 08:44:46 +0100

    Update SearchForClient.tsx

--- src/components/accountComponent/components/SearchForClient.tsx
+++ src/components/accountComponent/components/SearchForClient.tsx
@@ -5,7 +5,7 @@ import { useQuery } from 'react-query';
 export default function SearchForClient(props: any) {
  
     const { isLoading, data } = useQuery('SearchAllProfile', () =>
-        fetch('https://faadoli.herokuapp.com/api/v1/auth/profile/all', {
+        fetch('http://faad-env.eba-kfucwakm.eu-central-1.elasticbeanstalk.com/api/v1/auth/profile/all', {
             method: 'GET', // or 'PUT'
             headers: {
                 'Content-Type': 'application/json', 
