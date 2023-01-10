4b412e485ca3c520e32aa40a7af00e17b2b1e75b
egileekada <egileoniso.ekada@gmail.com>
2023-01-10 12:29:57 +0100

    update on search tank

--- src/components/reportComponent/Stock.tsx
+++ src/components/reportComponent/Stock.tsx
@@ -50,7 +50,7 @@ export default function Stock(props: any) {
 
         {!isLoading && (
             <>
-                {data.data.stocks.filter((item: any)=> new Date(item.date).getMonth() === new Date().getMonth() && new Date(item.date).getFullYear() === new Date().getFullYear() && item.isAdding === sold && item.product !== null && item.product.productCode === fuelType && tankID === item?.tank[0]?._id).map((item: any, index: any) => {   
+                {data?.data?.stocks.filter((item: any)=> new Date(item.date).getMonth() === new Date().getMonth() && new Date(item.date).getFullYear() === new Date().getFullYear() && item.isAdding === sold && item.product !== null && item.product.productCode === fuelType && tankID === item?.tank[0]?._id).map((item: any, index: any) => {   
                     stockDetail.splice(index, 1, item.level);  
                 })}
             </>
@@ -81,44 +81,48 @@ export default function Stock(props: any) {
 
     return (
         <div className='mt-6 w-full grid grid-cols-3 gap-6 ' >
-            {tankData.map((item: any, index: any)=> {
-                return(
-                    <div key={index} onClick={()=> ClickHandler(item.product.productCode)} className=' w-auto cursor-pointer bg-[#0A3977]  py-4 px-4 rounded-lg text-white ' >
-                        <div className=' pb-3 border-b border-[#0F56B3] ' >
-                            <p className=' font-Inter-Medium text-lg ' >{item.product.productCode} (This month)</p> 
-                            <div className='mt-2 flex' >
-                                <div className=' border-[#68A4F3] border-r pr-3 ' >
-                                    <p className=' font-Inter-SemiBold text-sm text-[#47FF47]  ' >In</p> 
-                                    {ThisMonthTankInfo(item._id, item.product.productCode, true)} 
-                                    {/* <p className=' font-Inter-SemiBold text-lg ' >{(item.capacity).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}ℓ</p> */}
+            {!isLoading && (
+                <> 
+                    {tankData.map((item: any, index: any)=> {
+                        return(
+                            <div key={index} onClick={()=> ClickHandler(item?.product?.productCode)} className=' w-auto cursor-pointer bg-[#0A3977]  py-4 px-4 rounded-lg text-white ' >
+                                <div className=' pb-3 border-b border-[#0F56B3] ' >
+                                    <p className=' font-Inter-Medium text-lg ' >{item?.product?.productCode} (This month)</p> 
+                                    <div className='mt-2 flex' >
+                                        <div className=' border-[#68A4F3] border-r pr-3 ' >
+                                            <p className=' font-Inter-SemiBold text-sm text-[#47FF47]  ' >In</p> 
+                                            {ThisMonthTankInfo(item._id, item?.product?.productCode, true)} 
+                                            {/* <p className=' font-Inter-SemiBold text-lg ' >{(item.capacity).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}ℓ</p> */}
+                                        </div>
+                                        <div className=' pl-3' > 
+                                            <p className=' font-Inter-SemiBold text-sm text-[#FF7070]  ' >Out</p> 
+                                            {ThisMonthTankInfo(item._id, item?.product?.productCode, false)} 
+                                            {/* <p className=' font-Inter-SemiBold text-lg ' >{(item.capacity - item.level).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}ℓ</p> */}
+                                        </div>
+                                    </div>   
+                                    <p className=' text-sm font-Inter-Regular mt-2 '>Closing Stock• {item?.level.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}ℓ</p>
                                 </div>
-                                <div className=' pl-3' > 
-                                    <p className=' font-Inter-SemiBold text-sm text-[#FF7070]  ' >Out</p> 
-                                    {ThisMonthTankInfo(item._id, item.product.productCode, false)} 
-                                    {/* <p className=' font-Inter-SemiBold text-lg ' >{(item.capacity - item.level).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}ℓ</p> */}
-                                </div>
-                            </div>   
-                            <p className=' text-sm font-Inter-Regular mt-2 '>Closing Stock• {item.level.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}ℓ</p>
-                        </div>
-                        <div className='pt-3' >
-                            <p className=' font-Inter-Medium text-lg ' >{item.product.productCode} (This year)</p> 
-                            <div className='mt-2 flex' >
-                                <div className=' border-[#68A4F3] border-r pr-3 ' >
-                                    <p className=' font-Inter-SemiBold text-sm text-[#47FF47]  ' >In</p>
-                                    {ThisYearTankInfo(item._id, item.product.productCode, true)} 
-                                    {/* <p className=' font-Inter-SemiBold text-lg ' >{(item.capacity).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}ℓ</p> */}
-                                </div>
-                                <div className=' pl-3' > 
-                                    <p className=' font-Inter-SemiBold text-sm text-[#FF7070]  ' >Out</p>
-                                    {ThisYearTankInfo(item._id, item.product.productCode, false)} 
-                                    {/* <p className=' font-Inter-SemiBold text-lg ' >{(item.capacity - item.level).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}ℓ</p> */}
+                                <div className='pt-3' >
+                                    <p className=' font-Inter-Medium text-lg ' >{item?.product?.productCode} (This year)</p> 
+                                    <div className='mt-2 flex' >
+                                        <div className=' border-[#68A4F3] border-r pr-3 ' >
+                                            <p className=' font-Inter-SemiBold text-sm text-[#47FF47]  ' >In</p>
+                                            {ThisYearTankInfo(item._id, item?.product?.productCode, true)} 
+                                            {/* <p className=' font-Inter-SemiBold text-lg ' >{(item.capacity).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}ℓ</p> */}
+                                        </div>
+                                        <div className=' pl-3' > 
+                                            <p className=' font-Inter-SemiBold text-sm text-[#FF7070]  ' >Out</p>
+                                            {ThisYearTankInfo(item._id, item?.product?.productCode, false)} 
+                                            {/* <p className=' font-Inter-SemiBold text-lg ' >{(item.capacity - item.level).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}ℓ</p> */}
+                                        </div>
+                                    </div>
+                                    <p className=' text-sm font-Inter-Regular mt-2 '>Closing Stock• {item?.level.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}ℓ</p>
                                 </div>
                             </div>
-                            <p className=' text-sm font-Inter-Regular mt-2 '>Closing Stock• {item.level.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}ℓ</p>
-                        </div>
-                    </div>
-                )
-            })}
+                        )
+                    })}
+                </>
+            )}
         </div>
     )
 } 
