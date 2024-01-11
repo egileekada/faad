c6d95d646d558d4d19ef09b983c986a3e2bef0c9
egileekada <45034221+egileekada@users.noreply.github.com>
2024-01-11 09:23:50 +0100

    Update Url.ts

--- src/assets/BasicUrl/Url.ts
+++ src/assets/BasicUrl/Url.ts
@@ -1,5 +1,5 @@
-const liveUrl = "https://faadoil.herokuapp.com/api/v1/"
-const domain = "https://faadoil.herokuapp.com"
+const liveUrl = "https://faadoil-8f8ec2c02e97.herokuapp.com/api/v1/"
+const domain = "https://faadoil-8f8ec2c02e97.herokuapp.com"
 
 export const BASEURL = {
     URL: liveUrl, 
